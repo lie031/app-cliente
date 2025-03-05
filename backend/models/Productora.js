35fbf21242bfa87696610e001109fd57bb323ce2
tkaMymas <=>
2025-03-05 18:11:56 -0500

    Cambio Productora

--- backend/models/Productora.js
+++ backend/models/Productora.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose')
 const ProductoraSchema = new mongoose.Schema({
   nombre: { type: String, required: true, unique: true },
   estado: { type: String, enum: ['Activo', 'Inactivo'], default: 'Activo' },
-  fecha_creacion: { type: Date, default: Date.now },
-  fecha_actualizacion: { type: Date, default: Date.now },
   slogan: { type: String, required: true },
   descripcion: { type: String, required: true }
 })
