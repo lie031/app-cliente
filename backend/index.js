5eb19913e83cd1d46aa7215705fdfbc30fc0a68b
@amianGO <damiangaviria8@gmail.com>
2025-03-12 22:41:58 -0500

    Reconfiguracion del proyecto

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,11 @@ const puerto = 3000
 obtenerConexion()
 app.use(express.json())
 
-app.use('/api/genero', require('./routes/genero'))
-app.use('/api/director', require('./routes/director'))
-app.use('/api/media', require('./routes/mediaRoute'))
-app.use('/api/productora', require('./routes/productoraRoutes'))
-app.use('/api/tipo', require('./routes/tipoRoute'))
+app.use('/generos', require('./routes/genero'))
+app.use('/directores', require('./routes/director'))
+app.use('/medias', require('./routes/mediaRoute'))
+app.use('/productoras', require('./routes/productoraRoutes'))
+app.use('/tipos', require('./routes/tipoRoute'))
 
 app.listen(puerto, () => {
   console.log(`app corriendo en el puerto ${puerto}`)
