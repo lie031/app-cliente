476b44efbf1a8aaf18c0bc9cb7542a38e5a2e989
lie03 <139715955+lie031@users.noreply.github.com>
2025-03-26 17:43:02 -0500

    ajustes en el navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
@@ -6,21 +5,12 @@ import MediaList from './Components/MediaList'
 import MediaForm from './Components/MediaForm'
 import { TypeView } from './Components/tipo/TypeView'
 import TypeForm from './Components/tipo/TypeForm'
-import { DirectorView } from './Components/director/DirectorView'
+import ProductoraList from './Components/productora/ProductoraList'
+import ProductoraForm from './Components/productora/ProductoraForm'
+import DirectorView from './Components/director/DirectorView'
 import DirectorForm from './Components/director/DirectorForm'
 import GeneroView from './Components/genero/GeneroView'
 import GeneroForm from './Components/genero/GeneroForm'
-=======
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import MediaList from './Components/MediaList';
-import MediaForm from './Components/MediaForm';
-import { TypeView } from './Components/tipo/TypeView';
-import TypeForm from './Components/tipo/TypeForm';
-import ProductoraList from './Components/productora/ProductoraList';
-import ProductoraForm from './Components/productora/ProductoraForm';
->>>>>>> 4c3768b4552d41597586e69aed9b3bb503d9dfae
 
 // Componentes para las otras paginas
 const Home = () => <div className='container'><h1>Pagina de Inicio</h1></div>
@@ -33,19 +23,27 @@ function App () {
         <main className='container'>
           <Routes>
 
-              <Route path='/' element={<Home/>}></Route>
-              
-              <Route path='/medias' element ={<MediaList/>} />
-              <Route path='/create' element ={<MediaForm/>} />
-              <Route path='/medias/:_id' element = {<MediaForm />} />
+            <Route path='/' element={<Home />} />
 
-              <Route path='/tipos' element = {<TypeView />} />
-              <Route path='/tipos/create' element = {<TypeForm />} />
-              <Route path='/tipos/:_id' element = {<TypeForm />} />
+            <Route path='/medias' element={<MediaList />} />
+            <Route path='/create' element={<MediaForm />} />
+            <Route path='/medias/:_id' element={<MediaForm />} />
 
-              <Route path='/productoras' element={<ProductoraList />} />
-              <Route path='/productoras/create' element={<ProductoraForm />} />
-              <Route path='/productoras/:_id' element={<ProductoraForm />} />
+            <Route path='/tipos' element={<TypeView />} />
+            <Route path='/tipos/create' element={<TypeForm />} />
+            <Route path='/tipos/:_id' element={<TypeForm />} />
+
+            <Route path='/productoras' element={<ProductoraList />} />
+            <Route path='/productoras/create' element={<ProductoraForm />} />
+            <Route path='/productoras/:_id' element={<ProductoraForm />} />
+
+            <Route path='/directores' element={<DirectorView />} />
+            <Route path='/directores/create' element={<DirectorForm />} />
+            <Route path='/directores/:_id' element={<DirectorForm />} />
+
+            <Route path='/generos' element={<GeneroView />} />
+            <Route path='/generos/create' element={<GeneroForm />} />
+            <Route path='/generos/:_id' element={<GeneroForm />} />
 
           </Routes>
         </main>
