c7ecb75f6a0728e7355041dc5b0f4825e75472bd
lie031 <lie032002@email.com>
2025-06-08 20:00:18 -0500

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Navbar from './Components/Navbar'
 import MediaList from './Components/MediaList'
 import MediaForm from './Components/MediaForm'
@@ -23,119 +24,121 @@ import PrivateRoute from './Components/auth/PrivateRoute'
 
 function App () {
   return (
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <main className='container'>
-          <Routes>
-            {/* Rutas públicas */}
-            <Route path='/login' element={<Login />} />
-            <Route path='/registro' element={<Registro />} />
-            <Route path='/registro-admin' element={<RegistroAdmin />} />
+    <AuthProvider>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <main className='container'>
+            <Routes>
+              {/* Rutas públicas */}
+              <Route path='/login' element={<Login />} />
+              <Route path='/registro' element={<Registro />} />
+              <Route path='/registro-admin' element={<RegistroAdmin />} />
 
-            {/* Rutas protegidas */}
-            <Route path='/' element={
-              <PrivateRoute>
-                <MediaList />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/create' element={
-              <PrivateRoute>
-                <MediaForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/medias/:_id' element={
-              <PrivateRoute>
-                <MediaForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/medias/detail/:_id' element={
-              <PrivateRoute>
-                <MediaDetail />
-              </PrivateRoute>
-            } />
+              {/* Rutas protegidas */}
+              <Route path='/' element={
+                <PrivateRoute>
+                  <MediaList />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/create' element={
+                <PrivateRoute>
+                  <MediaForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/medias/:_id' element={
+                <PrivateRoute>
+                  <MediaForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/medias/detail/:_id' element={
+                <PrivateRoute>
+                  <MediaDetail />
+                </PrivateRoute>
+              } />
 
-            <Route path='/tipos' element={
-              <PrivateRoute>
-                <TypeView />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/tipos/create' element={
-              <PrivateRoute>
-                <TypeForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/tipos/:_id' element={
-              <PrivateRoute>
-                <TypeForm />
-              </PrivateRoute>
-            } />
+              <Route path='/tipos' element={
+                <PrivateRoute>
+                  <TypeView />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/tipos/create' element={
+                <PrivateRoute>
+                  <TypeForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/tipos/:_id' element={
+                <PrivateRoute>
+                  <TypeForm />
+                </PrivateRoute>
+              } />
 
-            <Route path='/productoras' element={
-              <PrivateRoute>
-                <ProductoraList />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/productoras/create' element={
-              <PrivateRoute>
-                <ProductoraForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/productoras/:_id' element={
-              <PrivateRoute>
-                <ProductoraForm />
-              </PrivateRoute>
-            } />
+              <Route path='/productoras' element={
+                <PrivateRoute>
+                  <ProductoraList />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/productoras/create' element={
+                <PrivateRoute>
+                  <ProductoraForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/productoras/:_id' element={
+                <PrivateRoute>
+                  <ProductoraForm />
+                </PrivateRoute>
+              } />
 
-            <Route path='/directores' element={
-              <PrivateRoute>
-                <DirectorView />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/directores/create' element={
-              <PrivateRoute>
-                <DirectorForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/directores/:_id' element={
-              <PrivateRoute>
-                <DirectorForm />
-              </PrivateRoute>
-            } />
+              <Route path='/directores' element={
+                <PrivateRoute>
+                  <DirectorView />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/directores/create' element={
+                <PrivateRoute>
+                  <DirectorForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/directores/:_id' element={
+                <PrivateRoute>
+                  <DirectorForm />
+                </PrivateRoute>
+              } />
 
-            <Route path='/generos' element={
-              <PrivateRoute>
-                <GeneroView />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/generos/create' element={
-              <PrivateRoute>
-                <GeneroForm />
-              </PrivateRoute>
-            } />
-            
-            <Route path='/generos/:_id' element={
-              <PrivateRoute>
-                <GeneroForm />
-              </PrivateRoute>
-            } />
+              <Route path='/generos' element={
+                <PrivateRoute>
+                  <GeneroView />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/generos/create' element={
+                <PrivateRoute>
+                  <GeneroForm />
+                </PrivateRoute>
+              } />
+              
+              <Route path='/generos/:_id' element={
+                <PrivateRoute>
+                  <GeneroForm />
+                </PrivateRoute>
+              } />
 
-            {/* Redirección por defecto */}
-            <Route path='*' element={<Navigate to='/' replace />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+              {/* Redirección por defecto */}
+              <Route path='*' element={<Navigate to='/' replace />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
